42aef59727c3b2d6d3ef23278a22c4380abb9143
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2017-10-18 00:10:37 +0200

    rebuild reference api

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","AuthenticationService"],["c","Author"],["c","AuthorsService"],["c","Book"],["c","BookReviewSummary"],["c","BooksService"],["c","Database"],["c","DatabaseConnection"],["c","Defaults"],["c","LogsService"],["c","Review"]];
+ApiGen.elements = [["c","AuthenticationService"],["c","Author"],["c","AuthorsService"],["c","Book"],["c","BookReviewSummary"],["c","BooksService"],["c","Comment"],["c","CommentsService"],["c","Database"],["c","DatabaseConnection"],["c","Defaults"],["c","LogsService"],["c","Review"]];
